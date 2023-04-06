4a3a066a82944fb08ef7bea78aeeeb2d8eb3b2e6
Jessica Spivak <jls103193@yahoo.com>
2023-04-06 14:08:13 -0400

    got side tracked and added more stuff to about and javascript without switching branches

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
 //alert("hello");
+const externalLink = document.querySelector('#external_link');
+
+externalLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  const link = e.target.href;
+
+  const navigateAway = confirm("Are you sure you want to navigate away from this website?");
+
+  if(navigateAway) {
+    window.location = link;
+  }
+})
+
+const externalLink2 = document.querySelector('#external_link2');
+
+externalLink2.addEventListener('click', function(e) {
+  e.preventDefault();
+  const link = e.target.href;
+
+  const navigateAway = confirm("Are you sure you want to navigate away from this website?");
+
+  if(navigateAway) {
+    window.location = link;
+  }
+})
