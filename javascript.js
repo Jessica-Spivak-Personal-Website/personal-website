ed530d401e934fbd113a32117a104860148e9de0
Jessica Spivak <jls103193@yahoo.com>
2023-04-10 14:11:22 -0400

    form added to contact me

--- javascript.js
+++ javascript.js
@@ -26,3 +26,38 @@ externalLink2.addEventListener('click', function(e) {
     window.location = link;
   }
 })
+
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
+
+    }
+    )
+})
+
+let navbar = document.querySelectorAll(".navlink")
+let navContainer = document.querySelector("#nav")
+let hamburger = document.querySelector("#ham")
+
+if (window.innerWidth < 720) {
+    navContainer.classList.add("hide")
+    navbar.forEach((node) => node.classList.add("hide"))
+    hamburger.classList.remove("hide")
+} else {
+    navContainer.classList.remove("hide")
+    navbar.forEach((node) => node.classList.remove("hide"))
+    hamburger.classList.add("hide")
+}
